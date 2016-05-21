e8dd69b8777bc591e21abcf5dac4834d0a13df13
Davis Allie <davis525@live.com.au>
2016-05-21 15:58:51 +1000

    Initial Commit

---
+++ tvOS Focus Engine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tvOS Focus Engine
+//
+//  Created by Davis Allie on 21/05/16.
+//  Copyright © 2016 tutsplus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
